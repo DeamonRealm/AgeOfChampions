9036ba7b5b3a8cd8a57c4046bff1c81a62dba944
Joan Pareja <imbecilustotalus@gmail.com>
2017-03-21 10:01:12 +0100

    Task manager placeholders

---
+++ AoE_Project/Motor2D/TaskManager.h
@@ -0,0 +1,32 @@
+#ifndef __TASK_MANAGER__
+#define __TASK_MANAGER__
+
+
+#include <string>
+
+//Task class structure
+class Task
+{
+public:
+	Task();
+	~Task();
+
+
+
+private:
+	//sad
+	std::string name;
+
+
+};
+
+
+
+
+
+
+
+
+
+#endif // !__TASK_MANAGER__
+
