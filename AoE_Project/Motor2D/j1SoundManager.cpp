5b9dade5709ef2a9b9b77f9a64559a92179b25c7
Andreu <Andreu.rbj@gmail.com>
2017-03-24 16:05:11 +0100

    SoundManagerUpdated

--- AoE_Project/Motor2D/j1SoundManager.cpp
+++ AoE_Project/Motor2D/j1SoundManager.cpp
@@ -11,75 +11,9 @@
 #include "p2Log.h"
 #include "j1EntitiesManager.h"
 #include "j1Audio.h"
-///Animation Sprite Class -----------------------
-//Constructor ===============
-Fx::Fx(const int sound_id, const int loops) : id(sound_id), loops(loops)
-{
-
-}
-
-//Destructor ================
-Fx::~Fx()
-{
-}
-
-//Functionality =============
-
-/// ---------------------------------------------
 
 
-
-///Animation Class ------------------------------
-//Constructor =========================
-Sound::Sound()
-{
-
-}
-
-//Destructor ==========================
-Sound::~Sound()
-{
-	sound.clear();
-}
-
-
-
-//Functionality =======================
-void Sound::SetLoop(int loop_state)
-{
-	loops = loop_state;
-}
-
-void Sound::SetId(uint id)
-{
-	enum_id = id;
-}
-
-
-int Sound::GetLoop() const
-{
-	return loops;
-}
-
-
-const std::vector<int>* Sound::GetAllSouds() const
-{
-	return &sound;
-}
-
-uint Sound::GetId() const
-{
-	return enum_id;
-}
-
-void Sound::AddSprite(const int sound_id, const int loops)
-{
-	sound.push_back(sound_id);
-}
-/// ---------------------------------------------
-
-
-/// Animation Block Class -----------------------
+/// Sound Block Class -----------------------
 //Constructor =========================
 Sound_Block::Sound_Block(uint enum_id) :enum_id(enum_id)
 {
@@ -89,20 +23,14 @@ Sound_Block::Sound_Block(uint enum_id) :enum_id(enum_id)
 //Destructor ==========================
 Sound_Block::~Sound_Block()
 {
-	ClearAnimationBlocks();
+	
 }
 
 //Functionality =======================
-void Sound_Block::ClearAnimationBlocks()
+
+void Sound_Block::ClearSound()
 {
-	while (childs.size() > 0)
-	{
-		childs.back()->ClearAnimationBlocks();
-		childs.pop_back();
-
-	}
-
-	if (sound != nullptr)delete sound;
+	sound.clear();
 }
 
 void Sound_Block::SetId(uint id)
@@ -110,46 +38,30 @@ void Sound_Block::SetId(uint id)
 	enum_id = id;
 }
 
+void Sound_Block::SetSound(const int value)
+{
+	sound.push_back(value);
+}
+
+int Sound_Block::GetAudio(int index)
+{
+	return sound[index];
+}
+
+
+
+int Sound_Block::GetSoundSize()
+{
+	return sound.size();
+}
+
 uint Sound_Block::GetId() const
 {
 	return enum_id;
 }
 
-Sound* Sound_Block::GetAnimation() const
-{
-	return sound;
-}
 
-Sound_Block * Sound_Block::GetBlock(int index) const
-{
-	return childs.at(index);
-}
 
-uint Sound_Block::GetChildsNum() const
-{
-	return childs.size();
-}
-
-Sound_Block * Sound_Block::SearchId(uint id) const
-{
-	uint size = childs.size();
-	for (uint k = 0; k < size; k++)
-	{
-		if (childs[k]->GetId() == id)return childs[k];
-	}
-
-	return nullptr;
-}
-
-void Sound_Block::SetAnimation(const Sound * new_animation)
-{
-	if (new_animation != nullptr)sound = (Sound*)new_animation;
-}
-
-void Sound_Block::AddAnimationBlock(Sound_Block* new_animation_block)
-{
-	if (new_animation_block != nullptr)childs.push_back(new_animation_block);
-}
 /// ---------------------------------------------
 
 
@@ -184,13 +96,13 @@ bool j1SoundManager::PostUpdate()
 bool j1SoundManager::CleanUp()
 {
 	//Clean the unit blocks
-	uint size = sound_block.size();
+	uint size = sound_blocks.size();
 
 	for (uint k = 0; k < size; k++)
 	{
-		sound_block[k]->ClearAnimationBlocks();
+		sound_blocks[k]->ClearSound();
 	}
-	sound_block.clear();
+	sound_blocks.clear();
 	return true;
 }
 
@@ -203,38 +115,79 @@ SOUND_ATTACK_TYPE j1SoundManager::StrToSoundEnum(const char * str) const
 	return NO_ATTACK_SOUND;
 }
 
-SOUND_TYPE j1SoundManager::StrToAttackEnum(const char * str) const
-{
-	if (strcmp(str, "attack") == 0)		return ATTACK_SOUND;
-
-	return NO_SOUND;
-}
-
 
 //Functionality =======================
 bool j1SoundManager::LoadSoundBlock(const char* xml_folder)
 {
-	
+	//Load animations data from folders
+	//XML load
+	LOG("Loading: %s", xml_folder);
+	std::string load_folder = name + "/" + xml_folder;
+	pugi::xml_document sound_data;
+	if (!App->fs->LoadXML(load_folder.c_str(), &sound_data)) return false;
 
+
+	//Texture load
+	load_folder = name + "/" + sound_data.child("SoundAtlas").attribute("imagePath").as_string();
+	SDL_Texture* texture = App->tex->Load(load_folder.c_str());
+
+	//Load Animations data
+	//Node focused at any unit node
+	pugi::xml_node all_node = sound_data.child("SoundAtlas");
+
+	pugi::xml_node type_node;
+	//Node focused at any unit action node
+	pugi::xml_node sound_node;
+
+
+
+	type_node = all_node.first_child();
+
+	while (type_node != NULL)
+	{
+		Sound_Block* sound_block = new Sound_Block();
+
+		sound_block->SetId(StrToSoundEnum(type_node.attribute("enum").as_string()));
+
+		sound_node = type_node.first_child();
+		int fx = 0;
+		while (sound_node != NULL) {
+			load_folder = name + "/" + sound_node.attribute("name").as_string();
+			sound_block->SetSound(App->audio->LoadFx(load_folder.c_str()));
+			sound_node = sound_node.next_sibling();
+
+		}
+		sound_blocks.push_back(sound_block);
+		type_node = type_node.next_sibling();
+
+	}
 	return true;
 }
 
-bool j1SoundManager::UnitPlay(Unit* target)
-{
 
-	
-	return false;
+bool j1SoundManager::PlayAudio(SOUND_TYPE sound)
+{
+	bool ret = false;
+	time_t t;
+	srand(time(&t));
+	Sound_Block* block = nullptr;
+
+	uint size = sound_blocks.size();
+
+	for (uint k = 0; k < size; k++)
+	{
+		//Pointer to the current block
+		block = sound_blocks[k];
+
+		//Compare block unit id
+		if (block->GetId() == sound)
+		{
+			uint rand_sound = rand() % block->GetSoundSize();
+			ret=App->audio->PlayFx(rand_sound);
+			return ret;
+		}
+	}
+	return ret;
 }
 
-bool j1SoundManager::BuildingPlay(Building* target)
-{
 
-
-	return false;
-}
-
-bool j1SoundManager::ResourcePlay(Resource * target)
-{
-	
-	return false;
-}
--- AoE_Project/Motor2D/j1SoundManager.h
+++ AoE_Project/Motor2D/j1SoundManager.h
@@ -11,65 +11,7 @@ class Building;
 class Resource;
 enum SOUND_ATTACK_TYPE;
 enum SOUND_TYPE;
-///Animation Sprite Class -----------------------
-class Fx
-{
-public:
 
-	Fx(const int sound_id, const int loops);
-	~Fx();
-
-private:
-
-	
-	int						id = 255;
-	int						loops = 0;
-
-
-public:
-
-	//Functionality
-	//Get Methods
-	
-
-};
-/// ---------------------------------------------
-
-///Animation Class ------------------------------
-//Class that contains the animation basic necessary data
-class Sound
-{
-public:
-
-	Sound();
-
-	~Sound();
-
-private:
-
-	//Vector that storage the frames rect & pivot & z dimension
-	std::vector<int>		sound;
-	//Id of the animation enum type
-	uint					enum_id = 0;
-	//Current frame calculated by the timer
-	int						loops = 0;
-public:
-
-	//Functionality -------------------
-	//Set Methods -----------
-	void	SetLoop(int loops);
-	void	SetId(uint id);
-
-	//Get Methods -----------
-	int								GetLoop()const;
-	const std::vector<int>*			GetAllSouds()const;
-	uint							GetId()const;
-
-	//Add New sprite
-	void AddSprite(const int sound_id, const int loops);
-
-};
-/// ---------------------------------------------
 
 /// Animation Block Class -----------------------
 //Block that contains a vector of animations 
@@ -83,33 +25,24 @@ public:
 
 private:
 
-	//Vector of other animation blocks
-	std::vector<Sound_Block*>	childs;
 	//Enum id of this block
-	uint							enum_id = 0;
+	uint						enum_id = 0;
 	//Pointer to a vector of animations 
-	Sound*		sound = nullptr;
+	std::vector<int>		sound;
 
 public:
 
 	//Functionality -------------------
-	//Delete all contained blocks data
-	void	ClearAnimationBlocks();
-
+	void	ClearSound();
 	//Set Methods -----------
 	void	SetId(uint id);
-
+	void	SetSound(const int value);
 	//Get Methods -----------
+	int					GetAudio(int index);
+
+	int					GetSoundSize();
 	uint				GetId()const;
-	Sound*			GetAnimation()const;
-	Sound_Block*	GetBlock(int index)const;
-	uint				GetChildsNum()const;
-	Sound_Block*	SearchId(uint id)const;
-
-	//Add Methods -----------
-	void	SetAnimation(const Sound* new_animation);
-	void	AddAnimationBlock(Sound_Block* new_animation_block);
-
+	
 };
 /// ---------------------------------------------
 
@@ -139,14 +72,13 @@ public:
 private:
 
 	//Vectors that define all the entities animations
-	std::vector<Sound_Block*> sound_block;
+	std::vector<Sound_Block*> sound_blocks;
 
 public:
 
 	//Functionality -------------------
 	//Methods that transform strings to enums (used when loading data from xml)
 	SOUND_ATTACK_TYPE		StrToSoundEnum(const char* str)const;
-	SOUND_TYPE				StrToAttackEnum(const char* str)const;
 
 
 	//Blocks Load Methods ---
@@ -154,9 +86,8 @@ public:
 
 
 	//Animations Methods ----
-	bool UnitPlay(Unit* target);
-	bool BuildingPlay(Building* target);
-	bool ResourcePlay(Resource* target);
+	bool PlayAudio(SOUND_TYPE target);
+	
 
 };
 #endif // ! _J1SOUND_MANAGER_H_
