d4f827449fad3a0ffe9f1c8599dc68514f1c1eff
Andreu <Andreu.rbj@gmail.com>
2017-04-01 18:19:48 +0200

    baseEntities updated

--- AoE_Project/Motor2D/BaseEntities.h
+++ AoE_Project/Motor2D/BaseEntities.h
@@ -141,7 +141,6 @@ enum COLLISION_TYPE
 {
 	NO_COLLISION,
 	COLLISION_IDLE,
-	FUTURE_COLLISION_IDLE,
 	FUTURE_COLLISION_MOVE,
 	COLLISION_MOVE
 };
@@ -321,7 +320,7 @@ public:
 
 	//Actions ---------------
 	bool					Move(std::vector<iPoint>* path);
-	void					Repath(std::vector<iPoint>* path);
+	void					Repath(const iPoint & destination);
 	iPoint					FindWalkableCell(const iPoint& center);
 	bool					UnitHere();
 	void					Focus(const iPoint& target, bool play = true);
