2f75b6f0f0ed69ab1a58a9da7036cd16a222de4d
JoanPareja <joanparejapinyol@gmail.com>
2017-04-02 13:55:46 +0200

    AI simple worker

--- AoE_Project/Motor2D/j1AI.cpp
+++ AoE_Project/Motor2D/j1AI.cpp
@@ -1,16 +1,26 @@
-#include "j1AI.h"
 
+#include "j1App.h"
+#include "j1AI.h"
+#include "j1EntitiesManager.h"
+#include "j1FileSystem.h"
 
 
 j1AI::j1AI()
 {
+	name = "AI";
 }
 
 j1AI::~j1AI()
 {
 }
 
-bool j1AI::Awake(pugi::xml_node &)
+void j1AI::Enable()
+{
+
+	active = LoadEnemies("EnemiesSpawn.xml");
+}
+
+bool j1AI::Awake(pugi::xml_node&)
 {
 	
 	bool ret = true;
@@ -21,19 +31,17 @@ bool j1AI::Awake(pugi::xml_node &)
 bool j1AI::Start()
 {
 
-
 	return true;
 }
 
 bool j1AI::PreUpdate()
 {
 
-	return false;
+	return true;
 }
 
 bool j1AI::Update(float dt)
 {
-	if(1)
 
 	return true;
 }
@@ -45,4 +53,109 @@ bool j1AI::CleanUp()
 	return true;
 }
 
+bool j1AI::LoadEnemies(const char * folder)
+{
+	
+	//Load
+	LOG("--- Loading %s...", folder);
+	std::string load_folder = name + "/" + folder;
+	pugi::xml_document enemy_data;
+
+	if (!App->fs->LoadXML(load_folder.c_str(), &enemy_data))
+	{
+		LOG("Enemy AI load error!");
+		return false;
+	}
+
+	pugi::xml_node unit_node = enemy_data.child("data").child("units").first_child();
+	while (unit_node != NULL)
+	{
+		Unit* new_unit = App->entities_manager->GenerateUnit(App->animator->StrToUnitEnum(unit_node.attribute("type").as_string()), ENEMY);
+		new_unit->SetPosition(unit_node.attribute("pos_x").as_int(0), unit_node.attribute("pos_y").as_int(0));
+		unit_node = unit_node.next_sibling();
+	}
+
+	return true;
+}
+
+
+
+///AICommands managment---------------------------------------------------------------------------------------------
+//AiWokerk definition--------------------
+AIWorker::AIWorker()
+{
+}
+
+AIWorker::~AIWorker()
+{
+}
+
+void AIWorker::Update()
+{
+	if (current_command != nullptr)
+	{
+		if (current_command->execute())
+		{
+			delete current_command;
+			current_command = nullptr;
+		}
+	}
+	else if (!command_queue.empty())
+	{
+		current_command = command_queue.front();
+		command_queue.pop_front();
+		//Delete command if it can't be activated
+		if (!current_command->Activation())
+		{
+			delete current_command;
+			current_command = nullptr;
+		}
+	}
+
+}
+
+void AIWorker::AddAICommand(AICommand* command)
+{
+	command_queue.push_back(command);
+}
+
+void AIWorker::Reset()
+{
+	delete current_command;
+	current_command = nullptr;
+
+	AICommand* to_erase;
+
+	while (!command_queue.empty())
+	{
+		to_erase = command_queue.front();
+		command_queue.pop_front();
+		RELEASE(to_erase);
+	}
+}
+//---------------------------------------
+
+//WaitCommand definition-----------------
+WaitAICommand::WaitAICommand(uint time) : time(time)
+{
+}
+
+WaitAICommand::~WaitAICommand()
+{
+}
+
+bool WaitAICommand::Activation()
+{
+	timer.Start();
+	return true;
+}
+
+bool WaitAICommand::Execute()
+{
+	if (timer.Read() > time) return true;
+
+	return false;
+}
+//---------------------------------------
+///------------------------------------------------------------------------------------------------------------------
 
--- AoE_Project/Motor2D/j1AI.h
+++ AoE_Project/Motor2D/j1AI.h
@@ -4,6 +4,7 @@
 #include "j1Module.h"
 #include "p2Log.h"
 
+class Unit;
 
 class j1AI : public j1Module
 {
@@ -13,6 +14,8 @@ public:
 
 public:
 	void Init() { active = false; };
+
+	void Enable();
 
 	// Called before render is available
 	bool Awake(pugi::xml_node&);
@@ -32,9 +35,71 @@ public:
 
 private:
 
+	bool LoadEnemies(const char* folder);
+
+
+private:
+	std::list<Unit*> enemy_units;
 
 };
 
+
+///AI is controlled thorugh childs from this command (like ActionManager)--------
+class AICommand
+{
+public:
+	AICommand() {};
+	~AICommand() {};
+public:
+	//Returns false if Action was unable to initialize
+	virtual bool Activation() { return true; }
+	//Returns TRUE when execute finshed
+	virtual bool execute() { return true; };
+
+};
+///--------------------------------------
+
+
+
+///AI Worker-----------------------------
+class AIWorker
+{
+public:
+	AIWorker();
+	~AIWorker();
+
+private:
+	std::list<AICommand*> command_queue;
+	AICommand* current_command = nullptr;
+
+
+public:
+	void Update();
+	void AddAICommand(AICommand* command);
+	void Reset();
+
+};
+///------------------------------------
+
+
+///AI Commands-------------------------
+//Wait command
+class WaitAICommand
+{
+public:
+	WaitAICommand(uint time);
+	~WaitAICommand();
+
+public: 
+	bool Activation();
+	bool Execute();
+
+
+private:
+	uint time = 0;
+	j1Timer timer;
+
+};
 
 #endif // !_J1_AI_H_
 
